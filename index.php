e5912581fba60d8254f4369175e2ace1676ca012
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-12-12 17:09:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+    $word = 'ciao';
+    echo $word;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bad Words</title>
+</head>
+<body>
+
+    <div>
+        
+    </div>
+    
+</body>
+</html>
